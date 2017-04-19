3e5b67f0bd68b7049571b8459e310926b41e8109
louiehernandez95 <louieghernandez95@gmail.com>
2017-04-18 21:53:37 -0500

    final draft

--- Project_3/src/main/java/Drawing1.java
+++ Project_3/src/main/java/Drawing1.java
@@ -1,13 +1,12 @@
 
 public class Drawing1 implements Drawing {
 
-	public void drawCircle(double x_intercept, double y_intercept, double radius) {
-		System.out.println("Drawing a Circle");
-		
-	}
-
 	public void drawRectangle(double x_intercept1, double y_intercept1, double x_intercept2, double y_intercept2) {
-		System.out.println("Drawing a Line");
+		System.out.println("You are now drawing a Line");
+
+	}
+	public void drawCircle(double x_intercept, double y_intercept, double radius) {
+		System.out.println("You are now drawing a Circle");
 		
 	}
 
